4c09ac572be8c3556cc33842b134bd4c6eba38c4
Geoffrey Irving <irving@naml.us>
2024-05-26 11:12:40 +0100

    Strip down to interval arithmetic portion
    https://github.com/girving/interval is now a separate repo from ray.

---
+++ Interval.lean
@@ -0,0 +1,13 @@
+import Interval.Box
+import Interval.Division
+import Interval.Floating.Abs
+import Interval.Floating.Add
+import Interval.Floating.Basic
+import Interval.Floating.Neg
+import Interval.Floating.Order
+import Interval.Floating.Scale
+import Interval.Interval.Basic
+import Interval.Interval.Mul
+import Interval.Interval.Conversion
+import Interval.Series
+import Interval.Floating.Standardization
--- lakefile.lean
+++ lakefile.lean
@@ -12,33 +12,4 @@ package ray where
 require mathlib from git "https://github.com/leanprover-community/mathlib4"
 
 @[default_target]
-lean_lib Ray
-
-@[default_target]
-lean_exe gradient_test {
-  root := `Ray.Render.GradientTest
-  moreLinkArgs := #["-L/opt/homebrew/lib", "-lpng"]
-}
-
-@[default_target]
-lean_exe bad_mandelbrot {
-  root := `Ray.Render.BadMandelbrot
-  moreLinkArgs := #["-L/opt/homebrew/lib", "-lpng"]
-}
-
-@[default_target]
-lean_exe primes {
-  root := `Ray.Experimental.Primes
-}
-
-target png.o pkg : FilePath := do
-  let o := pkg.buildDir / "Ray/Render/png.o"
-  let src ← inputFile <| pkg.dir / "Ray/Render/png.cc"
-  let args := #["-I", (←getLeanIncludeDir).toString, "-I/opt/homebrew/include"]
-  buildO o src args #["-fPIC"] "c++" getLeanTrace
-
-extern_lib libray pkg := do
-  let name := nameToStaticLib "ray"
-  let png ← fetch <| pkg.target ``png.o
-  buildStaticLib (pkg.nativeLibDir / name) #[png]
-
+lean_lib Interval
